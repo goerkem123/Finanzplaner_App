2020078f5b726f2aabdbadfe41df12e2798bf0f4
Dein Name <goerkem.ardal38@gmail.com>
2025-11-14 22:06:41 +0100

    Firebase Login fertig implementiert

---
+++ app/src/main/java/com/example/finanzplaner/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.finanzplaner;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.mainText);
+        tv.setText("Du bist eingeloggt!");
+    }
+}

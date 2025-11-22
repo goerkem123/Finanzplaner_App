e784d5e5053a2d666056338387ed0fdbdfe156f8
emrullahonder-6327 <emrullahonder27@gmail.com>
2025-11-22 21:34:28 +0100

    Vom Transaktions Layout zum Home Layout wieder zurück durch den Button

--- app/src/main/java/com/example/finanzplaner/AddTransactionActivity.java
+++ app/src/main/java/com/example/finanzplaner/AddTransactionActivity.java
@@ -8,11 +8,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class AddTransactionActivity extends AppCompatActivity {
+
+    private FloatingActionButton fabSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_transaction);
+
+        fabSave = findViewById(R.id.fab_save_transaction);
+        fabSave.setOnClickListener(v -> {
+            finish();
+        });
     }
 }

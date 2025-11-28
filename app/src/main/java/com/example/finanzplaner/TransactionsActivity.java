aa4d8807c7a7dca27209a4a8aa69bbbce153db77
emrullahonder-6327 <emrullahonder27@gmail.com>
2025-11-28 10:25:12 +0100

    UI Elemente hinzugefügt

--- app/src/main/java/com/example/finanzplaner/TransactionsActivity.java
+++ app/src/main/java/com/example/finanzplaner/TransactionsActivity.java
@@ -2,23 +2,32 @@ package com.example.finanzplaner;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class TransactionsActivity extends AppCompatActivity {
+
+    // UI Elemente
+    private EditText etSearch;
+    private Spinner spinnerCategory;
+    private RecyclerView recyclerView;
+    private BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transactions);
 
-        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
+        bottomNav = findViewById(R.id.bottomNavigationView);
 
         // Hier ist "Transaktionen" der aktuelle Tab
         bottomNav.setSelectedItemId(R.id.nav_transactions);

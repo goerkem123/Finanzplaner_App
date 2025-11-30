44478c14a302d52ac95e8af879b43cf48d227f24
emrullahonder-6327 <emrullahonder27@gmail.com>
2025-11-30 17:04:53 +0100

    Variablen hinzugefügt und initialisiert

--- app/src/main/java/com/example/finanzplaner/ReportsActivity.java
+++ app/src/main/java/com/example/finanzplaner/ReportsActivity.java
@@ -2,17 +2,26 @@ package com.example.finanzplaner;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class ReportsActivity extends AppCompatActivity {
-
+    private Button btnGenerate;
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reports);
+
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+        btnGenerate = findViewById(R.id.btn_generate_pdf);
 
         BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
 
